cac557d84375c0fa28a6564cb3bb7f96a248ee65
narv1 <narv@onders.org>
2015-11-20 20:28:04 +0100

    library updatet

--- myString.h
+++ myString.h
@@ -1,8 +1,16 @@
 
+/******************************************/
+/*               strlength                */
+/******************************************/
+
 char strlength(){
   for( i = 0; array1[i] != '\0'; i++);
   return i-1;  
 }
+
+/******************************************/
+/*               strmirror                */
+/******************************************/
 
 char strmirror(){
   
@@ -17,4 +25,64 @@ char strmirror(){
   printf("\n%s", array2);
   printf("\n%s", array1);
   printf("\n\n");
+}
+
+/******************************************/
+/*               strsearch                */
+/******************************************/
+
+char strsearch(){
+
+  for( i = 0; array3[i] != '\0'; i++);
+  b = i;
+  b = b - 1;
+
+  int a = 0, j;
+
+  for( i = 0; i < l; i++){
+    if( b == 1){
+      printf("Sie haben leider nur einen einzelnen Buchstaben eingegeben.\nDas Programm endet jetzt!\n");
+      break;
+    }
+    if( l < b ){
+      printf("Das zu suchende Wort ist groeszer als der Text selbst.\nDas ist nicht moeglich.\nDas Programm beendet sich jetzt.\n");
+      break;
+    }
+    for( j = 0; j < b; j++){
+      if( array1[i] == array3[j] ){
+	if( array1[i+b-1] == array3[j+b-1] ){
+	  a++;
+	  i++;
+	}
+      }
+      if( b == a){
+	printf("\nDas Wort fängt an der Stelle %d und endet bei %d!", i-b+1, i );
+	printf("\nEs ist %d Buchstaben lang\n", b);
+	break;
+      }
+    }
+  }                                                    
+  
+}
+
+/******************************************/
+/*               strreplace               */
+/******************************************/ 
+
+char strreplace(){
+
+  for( i = 0; array4[i] != '\0'; i++);
+
+  //a = a - 1;
+
+  i = i - 1;
+
+  //b = a + i;
+
+  for( b = a + i; a - 1 < b; b--){
+
+    array1[b-1] = array4[i];
+    i--;
+  }                             
+  
 }
