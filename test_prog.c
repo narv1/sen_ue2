a9e22d9c011da8604b5f5a60b0e12338966047f9
narv1 <narv@onders.org>
2015-12-07 16:16:34 +0100

    patch

--- test_prog.c
+++ test_prog.c
@@ -32,19 +32,28 @@ int main (){
 
   printf("Dieses Programm testet die Bibliothek der 1.Uebung.\n");  //Erklärung
 
-  int prog = 6;
+  char prog = '0';
   char  test[N];
 
   printf("Welches Programm möchten sie ausfuehren?\nDruecken sie die\n1 fuer strlength\n2 fuer strmirror\n3 fuer strsearch\n4 fuer strreplace\n5 fuer strsubstr\n");
   // Schleife zum abfangen eines Fehlers
-  do{
-    prog = 0;
-    scanf("%d", &prog);
-  } while( prog < 1 || prog > 5 );
+  while( prog = '0' ){
+    prog = getchar();
+    getchar();
+    if( prog <= '5' && prog >= '1' ){
+      break;
+    }
+    printf("\nSie haben nichts zwischen 0-6 eingegeben!\n");
+  }
 
-  getchar(); //um im dem Speicherbuffer das vorhandene \n [welches scanf zurück lässt] zu löschen
-
-  // Befüllung des Arrays
+  /****************************/
+  /*  Bestätigung der Eingabe */
+  /****************************/
+  if( prog >= 1 || prog <= 2){
+    printf("Sie haben sich für die Nr. %c entschieden\n" , prog);
+  }
+  
+  // Befüllung des Arrays  
   printf("Geben sie einen gewünschten String ein: \n");
   fgets(test, N, stdin);
 
@@ -53,7 +62,7 @@ int main (){
   /*****************************/
   int i;
 
-  if( prog == 1){
+  if( prog == 49 ){
     // Ausgabe des Rückgabewertes
     i = strlength(test, N);
     printf("Die länge des arrays ist: %d\n", i-1);
@@ -62,7 +71,7 @@ int main (){
   /*************************/
   /*       strmirror       */
   /*************************/  
-  if ( prog == 2 ){
+  if ( prog == 50 ){
     //    strlength( test, N );
     i =strmirror(test, N);
   }
@@ -70,7 +79,7 @@ int main (){
   /*************************/
   /*       strsearch       */
   /*************************/
-  if( prog == 3 ){
+  if( prog == 51 ){
     i = strsearch(test, N);
   }
 
@@ -78,7 +87,7 @@ int main (){
   /*       strreplace      */
   /*************************/
   
-  if( prog == 4 ){
+  if( prog == 52 ){
     i =  strreplace(test, N);
   }
 
